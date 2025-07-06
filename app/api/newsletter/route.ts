f21aa0af4f15a99776f9473d1b796063e79fb082
jeongsehun <sehun.dev@gmail.com>
2025-07-06 21:57:41 +0900

    fix: 빌드 에러 수정 - newsletter 설정 비활성화

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -1,11 +1,12 @@
-import { NewsletterAPI } from 'pliny/newsletter'
-import siteMetadata from '@/data/siteMetadata'
+// 뉴스레터 기능 비활성화 - 추후 필요 시 주석 해제
+// import { NewsletterAPI } from 'pliny/newsletter'
+// import siteMetadata from '@/data/siteMetadata'
 
-export const dynamic = 'force-static'
+// export const dynamic = 'force-static'
 
-const handler = NewsletterAPI({
-  // @ts-ignore
-  provider: siteMetadata.newsletter.provider,
-})
+// const handler = NewsletterAPI({
+//   // @ts-ignore
+//   provider: siteMetadata.newsletter.provider,
+// })
 
-export { handler as GET, handler as POST }
+// export { handler as GET, handler as POST }
